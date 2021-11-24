7d784c53cc82e17b3e3c1b0ed0eb9185283ce666
Sanat Thakur <sanatthakur2002@gmail.com>
2021-11-24 21:50:12 +0530

    working on login page

--- connect1/form.py
+++ connect1/form.py
@@ -4,16 +4,22 @@ from django.contrib.auth.forms import AuthenticationForm
 from django.forms import widgets
 
 class RegistrationForm(AuthenticationForm):
+
+    # this is how we can give attr to the fields
     def __init__(self,*args,**kwargs):
         super().__init__(*args,**kwargs)
         self.fields['username'].widget.attrs.update({
-            'name':'username',
-            'class':'col-11','style':'border-radius: .5vw;height: 2.5vw;margin-bottom: 1.8vw; border-color: rgb(200, 202, 204);','placeholder':'Enter email'
+            'class':'col-11','style':'border-radius: .5vw;height: 2.5vw;margin-bottom: 1vw; border-color: rgb(200, 202, 204);','placeholder':'Enter email'
         })
+        self.fields['password'].widget.attrs.update({
+            'class':'col-11','style':'border-radius: .5vw;height: 2.5vw;margin-bottom: 1.6vw; border-color: rgb(200, 202, 204);','placeholder':'Password'
+        })
+
 
     class Meta:
         model = User
 
         fields = '__all__'
+        labels = {"username":"Userkname"}
+        
 
-        # widgets = {'username':forms.EmailInput(attrs={'class':'col-11','style':'border-radius: .5vw;height: 2.5vw;margin-bottom: 1.8vw; border-color: rgb(200, 202, 204);','placeholder':'Enter email'})}
--- connect1/views.py
+++ connect1/views.py
@@ -5,12 +5,14 @@ from .form import RegistrationForm
 # use your own customized form and just take the value from inputs and pass value to the django authentication form to authenticate 
 
 def login(request):
-    user = RegistrationForm()
+    # user = RegistrationForm()
     if request.method == "POST":
         name = request.POST.get("email")
         pas = request.POST.get("password")
         user = RegistrationForm(request.POST)
         if user.is_valid():
             print(name,pas)
-            user.save()
+            # user.save()
+    else:
+        user = RegistrationForm()
     return render(request,"login.html",{"form":user})
