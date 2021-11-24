d507e09c35dfb9c7d1ec77fae10c21d66a2ede12
Sanat Thakur <sanatthakur2002@gmail.com>
2021-11-24 19:11:04 +0530

    updated

--- connect1/views.py
+++ connect1/views.py
@@ -1,5 +1,14 @@
-from django.http.response import HttpResponse
+from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render
 
+# use your own customized form and just take the value from inputs and pass value to the django authentication form to authenticate 
+
 def login(request):
+    if request.method == "POST":
+        name = request.POST.get("email")
+        pas = request.POST.get("password")
+        user = UserCreationForm(username="ks",first_name="sanat",last_name="ksksss",email=name,password=pas)
+        if user.is_valid():
+            print(name,pas)
+            user.save()
     return render(request,"login.html")
