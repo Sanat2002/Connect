02fd5f22d8187d1c442ca0dda3195a5af1e7c1d0
Sanat Thakur <sanatthakur2002@gmail.com>
2021-11-25 20:21:12 +0530

    updated login page

--- connect1/form.py
+++ connect1/form.py
@@ -3,16 +3,16 @@ from django.contrib.auth.models import User
 from django.contrib.auth.forms import AuthenticationForm
 from django.forms import widgets
 
-class RegistrationForm(AuthenticationForm):
+class LoginForm(AuthenticationForm):
 
     # this is how we can give attr to the fields
     def __init__(self,*args,**kwargs):
         super().__init__(*args,**kwargs)
         self.fields['username'].widget.attrs.update({
-            'class':'col-11','style':'border-radius: .5vw;height: 2.5vw;margin-bottom: 1vw; border-color: rgb(200, 202, 204);','placeholder':'Enter email'
+            'class':'col-11','style':'border-radius: .5vw;height: 2.5vw;margin-bottom: 1vw; margin-left:.7vw; border-color: rgb(200, 202, 204);','placeholder':'Enter email'
         })
         self.fields['password'].widget.attrs.update({
-            'class':'col-11','style':'border-radius: .5vw;height: 2.5vw;margin-bottom: 1.6vw; border-color: rgb(200, 202, 204);','placeholder':'Password'
+            'class':'col-11','style':'border-radius: .5vw;height: 2.5vw;margin-bottom: 1.6vw; margin-left:.7vw; border-color: rgb(200, 202, 204);','placeholder':'Password'
         })
 
 
--- connect1/views.py
+++ connect1/views.py
@@ -1,18 +1,17 @@
 from django.contrib.auth.forms import UserCreationForm,AuthenticationForm
 from django.shortcuts import render
-from .form import RegistrationForm
+from .form import LoginForm
 
 # use your own customized form and just take the value from inputs and pass value to the django authentication form to authenticate 
 
 def login(request):
-    # user = RegistrationForm()
     if request.method == "POST":
         name = request.POST.get("email")
         pas = request.POST.get("password")
-        user = RegistrationForm(request.POST)
+        user = LoginForm(request.POST)
         if user.is_valid():
             print(name,pas)
             # user.save()
     else:
-        user = RegistrationForm()
+        user = LoginForm()
     return render(request,"login.html",{"form":user})
